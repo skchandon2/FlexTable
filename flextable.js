bc893c0389908c35807aa8dac1fc523fb3709026
Shah Chandon <skchandon@yahoo.com>
2020-11-09 20:47:55 -0500

    added getjsonval with possible concat function

--- flextable.js
+++ flextable.js
@@ -21,35 +21,35 @@
         });
     }  // end of function getData()
 
-        function createTable(inputData, strTemplateElementID) {
-            var $templateElement = $(strTemplateElementID);
-            $("#dynamicTable").empty();
-            $templateElement.hide();
-            var $childElems = $templateElement.find("li");
+    function createTable(inputData, strTemplateElementID) {
+        var $templateElement = $(strTemplateElementID);
+        $("#dynamicTable").empty();
+        $templateElement.hide();
+        var $childElems = $templateElement.find("li");
+        
+        var $tablex = $("<table/>").addClass("table table-striped table-bordered table-hover");
+        var $thead = $("<thead/>");
+        var $tbody = $("<tbody/>");
+        $tablex.append($thead);
+        $tablex.append($tbody);
+        row_count = inputData.length;
+        
+        var $hdrtr =  $("<tr/>");
+        $thead.append($hdrtr);
+        
+        populateHeaderCells($childElems, $hdrtr);
+
+
+        for (var r = 0; r < row_count; r++) {
+            var currentInputLine = inputData[r];
+            createAndPopulateOneLine($tbody, $childElems, currentInputLine);
             
-            var $tablex = $("<table/>").addClass("table table-striped table-bordered table-hover");
-            var $thead = $("<thead/>");
-            var $tbody = $("<tbody/>");
-            $tablex.append($thead);
-            $tablex.append($tbody);
-            row_count = inputData.length;
-            
-            var $hdrtr =  $("<tr/>");
-            $thead.append($hdrtr);
-         
-          populateHeaderCells($childElems, $hdrtr);
+
+        }//(End Of) for (var r=0; r<5; r++)...
 
 
-          for (var r = 0; r < row_count; r++) {
-              var currentInputLine = inputData[r];
-              createAndPopulateOneLine($tbody, $childElems, currentInputLine);
-              
-
-          }//(End Of) for (var r=0; r<5; r++)...
-
-
-            $("#dynamicTable").append($tablex);
-      }//(End Of Function createTable
+        $("#dynamicTable").append($tablex);
+    }//(End Of Function createTable
         
     function populateHeaderCells($childElemsx, $hdrtrx)
     {
@@ -76,21 +76,51 @@
     }//(End Of) populateHeaderCells
     
     function createAndPopulateOneLine($tbody, $childElems, currentInputLine)
+    {
+        var $trx = $("<tr/>").data("rowid", currentInputLine.id).click(function(e){
+                e.preventDefault(); 
+                var $trc = $(e.target).parent();
+                //console.log($(e.target));
+                alert($trc.data("rowid"))
+                });
+
+            $tbody.append($trx);
+            $.each($childElems, function(keyx, valx){
+                var curfieldname = $(valx).data("jsonfieldname");
+
+                jsonobj = getJsonValWithPossibleConcat(curfieldname, currentInputLine);
+                var $thcell1x = $("<td/>").html(jsonobj);
+                $trx.append($thcell1x);
+            });//(End Of) $.each
+
+    }//(End Of) function createAndPopulateOneLine()
+
+    function getJsonValWithPossibleConcat(curfieldnameWithPossiblePlus, currentJsonInputLine)
+    {
+        //check if the current field name has + (this would indicate concat request)
+        var curfieldnameSplitByPlusSign = curfieldnameWithPossiblePlus.split("+");
+        
+        var jsonobj = null;
+        
+        if(curfieldnameSplitByPlusSign.length>1)
         {
-            var $trx = $("<tr/>").data("rowid", currentInputLine.id).click(function(e){
-                    e.preventDefault(); 
-                    var $trc = $(e.target).parent();
-                    //console.log($(e.target));
-                    alert($trc.data("rowid"))
-                    });
 
-                $tbody.append($trx);
-                $.each($childElems, function(keyx, valx){
-                    var curfieldname = $(valx).data("jsonfieldname");
+            var concatres = "";
+            for(p=0;p<curfieldnameSplitByPlusSign.length; p++)
+            {
+                if(p>0)
+                {
+                    concatres +=", ";
+                }
 
-                    jsonobj = getJsonValWithPossibleConcat(curfieldname, currentInputLine);
-                    var $thcell1x = $("<td/>").html(jsonobj);
-                    $trx.append($thcell1x);
-                });//(End Of) $.each
+                concatres += getJsonVal(curfieldnameSplitByPlusSign[p], currentJsonInputLine);
+            }
+            jsonobj = concatres;
 
-        }//(End Of) function createAndPopulateOneLine()
+        }
+        else
+        {
+            jsonobj = getJsonVal(curfieldnameWithPossiblePlus, currentJsonInputLine);
+        }
+        return jsonobj;
+    }//(End Of) getJsonValWithConcat()
