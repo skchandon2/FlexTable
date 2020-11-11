e4c7a90b1790440daca5d6492f463cdd1e517c95
Shah Chandon <skchandon@yahoo.com>
2020-11-10 22:01:49 -0500

    fixed the sort

--- samples/books.php
+++ samples/books.php
@@ -1,14 +1,15 @@
 <?php
     header('Content-Type: application/json');
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+
     include 'dbconn.php';
 
-    $sort = isset($_REQUEST['sort']) ? $_REQUEST['sort'] : 'pubilshyear';
-
-
+    $sort = isset($_REQUEST['sort']) ? ($_REQUEST['sort'] != ""? $_REQUEST['sort']:'pubilshyear') : 'pubilshyear';    
 
     $result = array();
-
-
+    
     $recordset = mysql_query("select * from scbooks order by $sort") or die (mysql_error());
     while($row = mysql_fetch_object($recordset)){
         array_push($result, $row);
