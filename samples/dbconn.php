fdfcdb0f69831c1c1ddc6d3f622873bfe168f5f7
Shah Chandon <skchandon@yahoo.com>
2020-11-11 18:44:02 -0500

    removing dbconn settings file

--- samples/dbconn.php
+++ samples/dbconn.php
@@ -1,6 +1,6 @@
 <?php
 
-	include("dbconn_settings.php");
+	include("dbconfig.php");
 	$conn = mysql_connect($hostname, $username, $password)
 		or die("Unable to connect to MySQL");
 	//print "Connected to MySQL Test Database<br>";
