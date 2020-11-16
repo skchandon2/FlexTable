a9413c080ca55077797205b49e5aef774f7ec648
nnawreen <nrin2k@gmail.com>
2020-11-15 19:34:50 -0500

    Added pagination

--- samples/nntask.php
+++ samples/nntask.php
@@ -6,7 +6,9 @@
 try{
     include 'dbconn.php';
 
-    $sort = 'itemname';    
+    $sort = 'itemname'; 
+    $pagesizeStr = '5';
+    $curpageNumberStr = '1';   
 
     if(isset($_REQUEST['sort'])  )
     {
@@ -16,11 +18,19 @@ try{
         }
         
     }
-    
+
+    $pagesizeStr = $_REQUEST["pagesize"];
+    $curpageNumberStr = $_REQUEST["curpage"];
+
+    $pagesizeInt = intval($pagesizeStr);
+    $curpageNumberInt = intval($curpageNumberStr);
+
+    $recordStartIndex = ($curpageNumberInt - 1) * $pagesizeInt;
 
     $result = array();
     
-    $recordset = mysql_query("select * from nn_list order by $sort") or die ("{ error: " . mysql_error() . "}");
+    
+    $recordset = mysql_query("select * from nn_list order by $sort limit $recordStartIndex, $pagesizeInt") or die ("{ error: " . mysql_error() . "}");
     while($row = mysql_fetch_object($recordset)){
         array_push($result, $row);
     }
